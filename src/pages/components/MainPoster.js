6d5e2cb0af60d4bec3a3f23b264d964748596de3
LeeTH <kkllpp123456@naver.com>
2020-09-24 15:54:38 +0900

    MainPage Animation Update

--- src/pages/components/MainPoster.js
+++ src/pages/components/MainPoster.js
@@ -1,53 +1,103 @@
 import React,{useState, useEffect} from 'react'
 import "./css/MainPoster.css"   
 
+const poster1 =  require("../../img/poster1.png");
+const poster2 =  require("../../img/poster2.png");
+
 const MainPoster = () => {
-    const [animateText, setanimateText] = useState('animateText')
-    const [animateText2, setanimateText2] = useState('none')
-    const [animate3, setAnimate3] = useState('none')
+    const [animatePoster, setanimatePoster1] = useState('none')
+    const [animateText, setAnimateText] = useState('none')
+    const [animateText1, setanimateText1] = useState('none')
+    const [animate3, setAnimatePoster2] = useState('none')
+    const [width, setWidth] = useState(1000)
+    const [divice, setDevice] = useState('pc')
+    const [loading, setIsLoading] = useState(false);
+
 
 const onAnimateText = () =>{
-    setanimateText('animateText')
-    setanimateText2('animateText2')
+    setTimeout(()=>{
+        setanimatePoster1('poster1_on')
+    },300)
+    setTimeout(()=>{
+        setAnimateText('animate_text')
+    },1000)
+    setTimeout(()=>{
+        setanimateText1('animate_text1')
+    },1500)
 }
-// import styled from 'styled-components'
+const mobileCheck = () =>{
+    if(1600 < window.innerWidth){
+        setWidth(950)
+        setDevice('pc')
+    }
+    else if(1400 < window.innerWidth){ 
+        setWidth(750)
+    }
+    else if(1200 < window.innerWidth){ 
+        setWidth(550)
+    }
+    else if(1000 < window.innerWidth){ 
+        setWidth(300)
+    }
+    else{
+        setWidth(5)
+        setDevice('mob')
+        setTimeout(()=>{
+            setAnimatePoster2('poster2_on')
+        },300)
+    }
+}
 useEffect(() => {
-        onAnimateText();
+    mobileCheck();
+    onAnimateText();
+    setIsLoading(true)
 }, [])
 
+// 마우스 위치 알기
 function getCurrentScroll(){
     return  window.scrollY
-    }
+}
 
 document.addEventListener('scroll', () => {
     const currentScroll = getCurrentScroll()
-
-    if (625 <= currentScroll){
-        setAnimate3('animate_on')
-        setanimateText('none')
-        setanimateText2('none')
-    }else if(currentScroll <= 500){
-        setAnimate3('none')
-        setanimateText('animateText')
-        setanimateText2('animateText2')
+    if(divice == 'pc'){
+        if (width <= currentScroll){
+            setAnimatePoster2('poster2_on')
+            setanimatePoster1('none')
+            setanimateText1('none')
+        }else if(currentScroll <= width){
+            setAnimatePoster2('none')
+            setanimatePoster1('poster1_on')
+            setanimateText1('animate_text1')
+        }else{
+            return 0;
+        }
     }else{
-        return 0;
+        setanimatePoster1('poster1_on')
+        setAnimatePoster2('poster2_on')
     }
 })
+
     return (
-        <div>
+    <>            
+        {loading
+        ? <div>
             <div className={"poster1"}>
+                <img className={animatePoster} src={poster1} width="100%;"></img>
                 <div className={"animate_wrap"} >
-                    <p className={animateText}><span className={animateText2}>공약 </span>보다 더</p>
-                    <p className={animateText}>중요한건 <span className={animateText2}>실천 </span>입니다</p>
+                    <p className={animateText}><span className={animateText1}>공약</span> 보다 더</p>
+                    <p className={animateText}>중요한건 <span className={animateText1}>실천 </span>입니다</p>
                 </div>
             </div>
             <div className={"poster2"}>
                 <div  className={animate3}>
+                    <img src={poster2} width="100%;"></img>
                 </div>
             </div>
-            
         </div>
+        : "isLoading"
+        }
+    </>
     )
 }
 
