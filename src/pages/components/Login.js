28bcd8fe6793853e31113195841d87a36080889f
mnxmnz <codi0703@naver.com>
2020-09-27 13:07:14 +0900

    공지사항 수정

--- src/pages/components/Login.js
+++ src/pages/components/Login.js
@@ -1,30 +1,11 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import { Link } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
-
-export default function IconLabelButtons() {
-  const classes = useStyles();
-
-  return (
-    <div>
-      <Link to="/editor">
-        <Button
-          variant="contained"
-          color="default"
-          className={classes.button}
-          startIcon={<CloudUploadIcon />}
-        >
-          Upload
-        </Button>
-      </Link>
-    </div>
-  );
+import React, {useState, useEffect} from "react";
+const Login = (props) => {
+ 
+    return(
+        <div>
+          로그인 페이지
+        </div>
+    )
 }
+
+export default Login;
