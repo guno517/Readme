25fdc589e08aaec7460c3c024c0615cf364475eb
mnxmnz <codi0703@naver.com>
2020-09-28 22:54:22 +0900

    현재까지 notice 작업 백업

--- src/pages/components/Notice.js
+++ src/pages/components/Notice.js
@@ -24,8 +24,7 @@ const noticeposter =  require("../../img/Notice.png");
 //공지사항 테이블 상단 제목 작성자 작성일의 배경색, 글자색 및 테이블 폰트 크기 지정
 const StyledTableCell = withStyles((theme) => ({
   head: {
-    backgroundColor: theme.palette.info.light,
-    color: theme.palette.common.white,
+    color: theme.palette.info.light,
   },
 }))(TableCell);
 
@@ -100,48 +99,9 @@ TablePaginationActions.propTypes = {
     rowsPerPage: PropTypes.number.isRequired,
   };
 
-const rows = [
-    {
-      "index": 1,
-      "img": "https://t1.daumcdn.net/cfile/tistory/994BEF35",
-      "attachment": null,
-      "title": "코로나로 인한 비대면 수업 실시",
-      "content": "COVID-19로 인하여 1~5주차는 비대면으로 WEBEX 화상 강의로 대체하겠습니다.",
-      "writer": "가천대학교",
-      "time": "2020-09-13T00:00:00.000Z"
-    },
-    {
-      "index": 2,
-      "img": "https://www.gachon.ac.kr/Files/editupload/202",
-      "attachment": null,
-      "title": "2020-2학기 취업동아리 참여자 모집",
-      "content": null,
-      "writer": "가천대학교",
-      "time": "2020-09-13T00:00:00.000Z"
-    },
-    {
-      "index": 3,
-      "img": null,
-      "attachment": null,
-      "title": "2020-2학기 3주차~5주차 이후 수업운영 기준 안내",
-      "content": "코로나-19 교내 감염을 방지하고자 아래와 같이 2020-2학기 3주차~5주차 이후 수업운영 기준을 아래와 같이 공지하오니 참고바랍니다.\n\n \n\n1. 이론강좌 수업진행 기준\n\n   가. 1주차~5주차: 전체 비대면 실시간 화상강의\n\n   나. 6주차 이후: 아래의 2020-2학기 수업진행 기준에 따라 운영 (10월5일부터)\n\n        1) 61명 이상 강좌: 2학기 전체 비대면 실시간 화상강의\n\n        2) 31명~60명 강좌: 강의실에서 대면강의와 실시간 화상강의 동시 진행\n\n                                 ※학생의 자율적 선택권(대면 또는 비대면) 보장\n\n        3) 30명 이하 강좌: 강의실에서 대면강의\n\n \n\n2. 실험/실습/실기강좌 수업진행 기준 (정부의 방역 기준 철저 준수)\n\n   가. 1주차~2주차: 비대면 실시간 화상강의\n\n   나. 3주차 이후: 1학기와 같이 소규모 분반 운영 (9월14일부터)\n\n    ※ 지방학생 대상 실험/실습/실기 집중캠프 진행 예정(학기종료 직후 1주일, 12월21~27일)\n\n \n\n3. 기타사항\n\n   - 전체 대면강의 수업진행시 감염상황에 따라 사회적거리두기 준수\n\n   - 수업중 감염 발생을 차단하기 위하여 마스크착용(필수), 강의실 수시 환기 등 교수 및         학생의 수칙 준수",
-      "writer": "가천대학교",
-      "time": "2020-09-13T00:00:00.000Z"
-    },
-    {
-      "index": 4,
-      "img": null,
-      "attachment": null,
-      "title": "test",
-      "content": "content",
-      "writer": "가천대학교",
-      "time": "2020-09-13T00:00:00.000Z"
-    }
-  ]
-
 const useStyles2 = makeStyles({
   table: {
-    minWidth: 480,
+    minWidth: 300,
   },
 });
 
@@ -149,8 +109,6 @@ export default function CustomPaginationActionsTable() {
   const classes = useStyles2();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -164,7 +122,7 @@ export default function CustomPaginationActionsTable() {
   const [noticeData, setNoticeData] = useState(['']);
 
   const fetchApi = async() =>{
-    await fetch("http://ec2-3-34-192-67.ap-northeast-2.compute.amazonaws.com:5000/notice")
+    await fetch("http://ec2-3-34-192-67.ap-northeast-2.compute.amazonaws.com:3000/notice")
       .then((response) =>{
         if(response.status === 200){
           response.json()
@@ -187,13 +145,12 @@ export default function CustomPaginationActionsTable() {
            id:3
        })
   }, [])
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, noticeData.length - page * rowsPerPage);
   
   return (
       <TableContainer>
-        <div>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <img width="100%" src={noticeposter}></img>
-        </div>
+        <img id="noticeposter" src={noticeposter}></img>
         <Table className={classes.table} aria-label="custom pagination table">
           <TableHead id="tablehead">
             <TableRow>
@@ -205,8 +162,8 @@ export default function CustomPaginationActionsTable() {
           </TableHead>
           <TableBody>
             {(rowsPerPage > 0
-              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              : rows
+              ? noticeData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : noticeData
             ).map((row) => (
               <TableRow key={row.index}>
                 <TableCell style={{ width: 30 }} align="center">
@@ -230,13 +187,13 @@ export default function CustomPaginationActionsTable() {
             )}
             <TableFooter>
                 <TableRow>
-                    <div>
+                    <div id = "noticediv">
                         <Link to="/editor">
                             <Button
+                                id = "uploadbutton"
                                 variant="contained"
                                 color="default"
                                 className={classes.button}
-                                startIcon={<CloudUploadIcon />}
                             >
                             Upload
                             </Button>
