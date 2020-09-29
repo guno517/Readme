1a40df30b1c9ae0db7131fd621e1cb77278f5955
guno517 <60571418+guno517@users.noreply.github.com>
2020-09-29 10:45:24 +0900

    회원가입, 게시글작성

--- src/server/test.js
+++ src/server/test.js
@@ -3,9 +3,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const router = express.Router();
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// app.use((req, res, next) => { 
+//   res.header("Access-Control-Allow-Origin", "*") 
+//   res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if(req.method === 'OPTIONS'){
+//       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET'); 
+//       return res.status(200).json({});
+//   }
+//   next();
+// })
 
 let db_config = {
   host: "readme.crmkq7ncuwo3.ap-northeast-2.rds.amazonaws.com",
@@ -48,6 +68,29 @@ app.use('/notice', (req, res) => {
   });
 })
 
+app.use('/insert/notice', (req, res) => {
+  console.log("게시글 db에 저장");
+  let img = req.body['img'];
+  let attachment = req.body['attachment'];
+  let title = req.body['title'];
+  let content = req.body['content'];
+  let writer = req.body['writer'];
+  let time = req.body['time'];
+  connection.query('insert into notice values(?,?,?,?,?,?,?,?)', ['', '', '', title, content, writer, time, 1], function (err, rows, fields) {
+    res.redirect('/notice'); // or 상세페이지
+  })
+})
+
+app.use('/update/notice', (req, res) => {
+  console.log("게시글 수정 완료");
+  connection.query('update')
+})
+
+app.use('delete/notice', (req, res) => {
+  console.log('게시글 삭제 완료');
+
+})
+
 app.use('/activity', (req, res) => {
   let sql = 'SELECT * FROM activity';
 
@@ -56,15 +99,16 @@ app.use('/activity', (req, res) => {
   });
 })
 
-router.get('/', function (req, res, next) {
-  res.render('login');
-});
+router.get('/login', function (req, res, next) {
+  res.render(longin);
+})
 
 //router.post()
 app.use('/login', function (req, res, next) {
   let userId = req.body['id'];
   let userPw = req.body['pass'];
-  connection.query('select * from member where id=\'' + userId + '\' and password=\'' + userPw + '\'', function (err, rows, fields) {
+  console.log(req.body)
+  connection.query('select id,college,department,student_number,authority from member where id=\'' + userId + '\' and password=\'' + userPw + '\'', function (err, rows, fields) {
     if (!err) {
       if (rows[0] != undefined) {
         res.json({ user: rows });
@@ -78,39 +122,44 @@ app.use('/login', function (req, res, next) {
   });
 });
 
-router.get('/', function (req, res, next) {
-  res.render(join)
-})
+function checkId(userId) {
+  if (checkId) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-app.use('/join', function (req, res, next) {
+app.use('/signup', function (req, res, next) {
   // let userId = req.body['id'];
   // let userPw = req.body['pass'];
-  // let userPwRe = req.body['userPwRe'];
+  // let userPwCheck = req.body['userPwCheck'];
+  // let name = req.body['name'];
   // let college = req.body['college'];
   // let department = req.body['department'];
   // let student_number = req.body['student_number'];
-  // let phone = req.body['phone'];
-  let index = 3;
-  let userId = "test1";
-  let userPw = "test1";
-  let userPwRe = "test1";
-  let college = "test1";
-  let department = "test1";
-  let student_number = "123456789";
-  let phone = "010-0000-1111";
-  let authority = 0;
-  if (userPw == userPwRe) {
-    connection.query('insert into member values(?,?,?,?,?,?,?,?)', [index, userId, userPw, college, department, student_number, phone, null], function (err, rows, fields) {
+
+  let userId = "test1"
+  let userPw = "test1"
+  let userPwCheck = "test1"
+  let name = "test"
+  let college = "test"
+  let department = "test"
+  let student_number = 201611111
+
+  if (userPw == userPwCheck) {
+    connection.query('insert into member values(?,?,?,?,?,?,?,?)', ['', userId, userPw, name, college, department, student_number, ''], function (err, result) {
       if (!err) {
-        res.json({ user: rows });
+          res.send('회원가입성공');
+          console.log(req.body);
       } else {
-        res.send('err:' + err);
+        res.send('error:' + err);
+        console.log(err);
       }
     });
   } else {
     res.send("password is not match")
   }
-
 })
 
 router.get('/', function (req, res, next) {
@@ -149,13 +198,13 @@ app.use('/update', function (req, res, next) {
   })
 })
 
-  app.get('/home', function (req, res) {
-    res.send('hello world')
-  })
+app.get('/home', function (req, res) {
+  res.send('hello world')
+})
 
-  app.listen(5000, () => {
-    console.log('server connected!')
-  })
+app.listen(3000, () => {
+  console.log('server connected!')
+})
 
 
 
