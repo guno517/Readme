89c765a3f8b99bacd8aa45034edb6244f63cf505
mnxmnz <codi0703@naver.com>
2020-10-06 02:35:07 +0900

    공지사항 수정 진행중

--- src/pages/components/Notice.js
+++ src/pages/components/Notice.js
@@ -1,27 +1,18 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import "./css/Notice.css";
 import { useDispatch } from "react-redux";
-
-import { withStyles, makeStyles, useTheme } from "@material-ui/core/styles";
-
+import TablePaginationActions from "./NoticePagenation"
+import "./css/Notice.css";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import TableContainer from "@material-ui/core/TableContainer";
 import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
 import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
 import TableFooter from "@material-ui/core/TableFooter";
 import TablePagination from "@material-ui/core/TablePagination";
-import TableRow from "@material-ui/core/TableRow";
-import IconButton from "@material-ui/core/IconButton";
 import Button from "@material-ui/core/Button";
-
-import FirstPageIcon from "@material-ui/icons/FirstPage";
-import LastPageIcon from "@material-ui/icons/LastPage";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import { SlowMotionVideo } from "@material-ui/icons";
 
 const NoticePoster = require("../../img/Notice.png");
 
@@ -34,70 +25,30 @@ const StyledTableCell = withStyles((theme) => ({
 }))(TableCell);
 
 const useStyles = makeStyles((theme) => ({
-    button: {
+    root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(0),
+    },
+    search: {
         margin: theme.spacing(1),
         marginRight: "10%",
         display: "block",
         float: "right",
-        backgroundColor:'#59AAEB',
-        color:'white',
-    },
-    root: {
-        flexShrink: 0,
-        marginLeft: theme.spacing(0),
     },
     table: {
         width: "80%",
         minWidth: 200,
         margin: "0 auto",
     },
+        button: {
+        margin: theme.spacing(1),
+        marginRight: "10%",
+        display: "block",
+        float: "right",
+        backgroundColor: "#59AAEB",
+        color: "white",
+    },
 }));
-
-function TablePaginationActions(props) {
-    const classes = useStyles();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    const handleFirstPageButtonClick = (event) => {
-        onChangePage(event, 0);
-    };
-
-    const handleBackButtonClick = (event) => {
-        onChangePage(event, page - 1);
-    };
-
-    const handleNextButtonClick = (event) => {
-        onChangePage(event, page + 1);
-    };
-
-    const handleLastPageButtonClick = (event) => {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
-
-    return (
-        <div className={classes.root}>
-            <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="first page">
-                {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-                {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton onClick={handleNextButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="next page">
-                {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton onClick={handleLastPageButtonClick} disabled={page >= Math.ceil(count / rowsPerPage) - 1} aria-label="last page">
-                {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
-TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onChangePage: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
-};
 
 export default function CustomPaginationActionsTable() {
     const classes = useStyles();
@@ -138,40 +89,40 @@ export default function CustomPaginationActionsTable() {
     }, []);
 
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, noticeData.length - page * rowsPerPage);
-        
-        const [title, setTitle] = useState('');
-        
-        const onChangeValue = (e) =>{
-            setTitle(e.target.value);
-        }
-    
-        const searchActive = async() =>{
-            const response = await fetch('http://ec2-3-34-192-67.ap-northeast-2.compute.amazonaws.com:3000/notice/search',{
-                method:"POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body:JSON.stringify({
-                    title:title
-                })
-            })
-            .then(async(response)=>{
-                let response_json = await response.json();
-                setNoticeData(response_json.notice_search)    
-            })
-        }
-        
+
+    const [title, setTitle] = useState("");
+
+    const onChangeValue = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const searchActive = async () => {
+        const response = await fetch("http://ec2-3-34-192-67.ap-northeast-2.compute.amazonaws.com:3000/notice/search", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: title,
+            }),
+        }).then(async (response) => {
+            let response_json = await response.json();
+            setNoticeData(response_json.notice_search);
+        });
+    };
+
     return (
         <TableContainer className={classes.container}>
             <img id="NoticePoster" src={NoticePoster}></img>
-            <div style={{ width:'80%', margin:'0 auto', textAlign:'right'}}>
-                <div style={{display:'inline-block', width:'100%', marginTop:"100px", marginBottom:"3%"}}>
-                    <span style={{fontSize:'18px', marginRight:"10px"}}>내용: </span>
-                    <input onChange={onChangeValue} type="text" style={{width:'200px',height:'30px',fontSize:'18px'}}></input>
-                    <button onClick={searchActive} style={{fontSize:'18px', marginLeft:"20px", height:'38px', backgroundColor:'white', border:'1px solid #bbbbbb', verticalAlign:'bottom'}}>검색</button>
+            <div id="Search" className={classes.search}>
+                <div style={{ display: "inline-block", width: "100%", marginTop: "50px", marginBottom: "3%" }}>
+                    <span style={{ fontSize: "18px", marginRight: "10px" }}>제목: </span>
+                    <input onChange={onChangeValue} type="text" style={{ width: "100px", height: "30px", fontSize: "18px" }}></input>
+                    <button onClick={searchActive} style={{ fontSize: "18px", marginLeft: "20px", height: "38px", backgroundColor: "white", border: "1px solid #bbbbbb", verticalAlign: "bottom" }}>
+                        검색
+                    </button>
                 </div>
             </div>
-
             <Table className={classes.table} id="NoticeTable" aria-label="custom pagination table">
                 <TableHead>
                     <TableRow>
@@ -195,7 +146,7 @@ export default function CustomPaginationActionsTable() {
                                 {row.index}
                             </TableCell>
                             <TableCell component="th" scope="row">
-                                <Link id={row.index} to = {`/notice_detail/${row.index}`}>
+                                <Link id={row.index} to={`/notice_detail/${row.index}`}>
                                     {row.title}
                                 </Link>
                             </TableCell>
