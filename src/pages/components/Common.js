2be51fe0051758feac415f4830f4151aefe7f42b
LeeTH <kkllpp123456@naver.com>
2020-10-06 17:18:21 +0900

    리덕스 오류 해결

--- src/pages/components/Common.js
+++ src/pages/components/Common.js
@@ -2,9 +2,9 @@ import React, {useState, useEffect} from "react";
 import { Link } from 'react-router-dom';
 
 const LinkComponent = (props) => {
-    const {id, link, name, isActive, activeColor, isNotActiveColor} = props
+    const {id, onClick, link, name, isActive, activeColor, isNotActiveColor} = props
     return(
-            <li><Link to={link} style={{color:isActive ? activeColor : isNotActiveColor}} >{name}</Link></li>
+            <li onClick={onClick} ><Link to={link} style={{color:isActive ? activeColor : isNotActiveColor}} >{name}</Link></li>
     )
 }
 
