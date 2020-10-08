9bbfc19ba4b59592a3de379fd383b862c2657ca6
mnxmnz <codi0703@naver.com>
2020-10-08 18:05:33 +0900

    학생회 페이지 파이 차트 완료

---
+++ src/pages/components/CouncilChart.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Highcharts from "highcharts/highstock";
+import PieChart from "highcharts-react-official";
+
+const options = {
+    chart: {
+        type: "pie",
+    },
+    title: {
+        text: "총학생회 공약 이행 현황",
+    },
+    series: [
+        {
+            name: "Brands",
+            colorByPoint: true,
+            data: [
+                {
+                    name: "공약 개수",
+                    y: 61.41,
+                },
+                {
+                    name: "이행 완료",
+                    y: 11.84,
+                },
+            ],
+        },
+    ],
+};
+
+function CouncilChart() {
+    return (
+        <div>
+            <PieChart highcharts={Highcharts} options={options}></PieChart>
+        </div>
+    );
+}
+
+export default CouncilChart;
