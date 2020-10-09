79701cd60d8ebd40d65889e46bc5bb107f6850aa
mnxmnz <codi0703@naver.com>
2020-10-10 01:55:38 +0900

    학생회 페이지 공약 관리 버튼

--- src/pages/components/Council.js
+++ src/pages/components/Council.js
@@ -1,51 +1,36 @@
-import React, {useState, useEffect} from "react";
-import { useDispatch } from 'react-redux'
-import CouncilChart from './CouncilChart'
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import CouncilChart from "./CouncilChart";
+import CouncilCarousel from "./CouncilCarousel";
+import CouncilButton from "./CouncilButton";
 
 const Council = (props) => {
+    const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-    
-    useEffect(()=>{
+    useEffect(() => {
         dispatch({
-            type:'UPDATE_MENU',
-            id:1,
-            name:"학생회"
-        })
-     })
-     console.log(CouncilChart);
-    return(
+            type: "UPDATE_MENU",
+            id: 1,
+            name: "학생회",
+        });
+    });
+    return (
         <div>
-            {/* <div class="dropdown">
-                <button onClick={dropdownFunction}>
-                    단과대학
-                </button>
-                    <Link to={'/council/'}>IT융합대학</Link>
-                
+            <div>
                 드롭박스
-            </div> */}
-            <div>
-                <CouncilChart></CouncilChart>
-                <div>
-                    공약 현황 막대 그래프
-                </div>
             </div>
             <div>
-                공약 목록 버튼
+                <CouncilChart />
+            </div>
+            <div>공약 목록 버튼</div>
+            <div>
+                <CouncilCarousel />
             </div>
             <div>
-                이행 인증 공약 캐러셀
-            </div>
-            <div>
-                <div>
-                    공약 등록 버튼
-                </div>
-                <div>
-                    이행 인증 버튼
-                </div>
+                <CouncilButton />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Council;
---
+++ src/pages/components/CouncilButton.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import "./css/CouncilButton.css";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    buttondiv: {
+        width: "51%",
+        margin: "0 auto",
+        padding: "1%"
+    },
+    button: {
+        backgroundColor: "#59AAEB",
+        color: "white",
+    },
+}));
+
+function CouncilButton() {
+    const classes = useStyles();
+    return (
+        <div id="buttondiv" className={classes.buttondiv}>
+            <Link to="#">
+                <Button id="Button" className={classes.button} variant="contained">
+                    이행 인증
+                </Button>
+            </Link>
+            <Link to="#">
+                <Button id="Button" className={classes.button} variant="contained">
+                    공약 등록
+                </Button>
+            </Link>
+        </div>
+    );
+}
+
+export default CouncilButton;
